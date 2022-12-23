2ee5cc469c52cb12502c519863a43da71d470ddd
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-23 23:49:13 +0600

    6. Displaying Simple HTML

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("HI There");
+    res.send(`
+    <div>
+        <form>
+            <input placeholder="email" />
+            <input placeholder="password" />
+            <input placeholder="password confirmation" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `
+    );
 });
 
 app.listen(3000, () => {
