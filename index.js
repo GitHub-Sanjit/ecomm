a137e70f804de6a2a178b786db0aebbe013f2aa5
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-23 23:33:43 +0600

    4. Creating a Web Server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi There");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("HI There");
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on port ${3000}`);
+});
