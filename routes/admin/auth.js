4aa4e1d51de2d57575a8628d4a48525dfc094fa9
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-27 14:33:58 +0600

    8. Receiving Validation Output

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -8,30 +8,41 @@ const router = express.Router();
 
 router.get(
   "/signup",
-  [check("email").isEmail(), check("password"), check("passwordConfirmation")],
+
   (req, res) => {
     res.send(signupTemplate({ req }));
   }
 );
 
-router.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
+router.post(
+  "/signup",
+  [
+    check("email").trim().normalizeEmail().isEmail(),
+    check("password").trim().isLength({ min: 4, max: 20 }),
+    check("passwordConfirmation").trim().isLength({ min: 4, max: 20 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email already in use");
+    const { email, password, passwordConfirmation } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+      return res.send("Email already in use");
+    }
+
+    if (password !== passwordConfirmation) {
+      return res.send("Passwords must match");
+    }
+
+    const user = await usersRepo.create({ email, password });
+
+    req.session.userId = user.id;
+
+    res.send("Account Created");
   }
-
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-
-  const user = await usersRepo.create({ email, password });
-
-  req.session.userId = user.id;
-
-  res.send("Account Created");
-});
+);
 
 router.get("/signout", (req, res) => {
   req.session = null;
