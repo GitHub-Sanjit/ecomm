998a458b0e747bcc5eec9cf7f41ad2cc3d7981fa
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-27 14:47:24 +0600

    9. Adding Custom Validators

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -6,35 +6,38 @@ const signinTemplate = require("../../views/admin/auth/signin");
 
 const router = express.Router();
 
-router.get(
-  "/signup",
-
-  (req, res) => {
-    res.send(signupTemplate({ req }));
-  }
-);
+router.get("/signup", (req, res) => {
+  res.send(signupTemplate({ req }));
+});
 
 router.post(
   "/signup",
   [
-    check("email").trim().normalizeEmail().isEmail(),
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .custom(async (email) => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+          throw new Error("Email already in use");
+        }
+      }),
     check("password").trim().isLength({ min: 4, max: 20 }),
-    check("passwordConfirmation").trim().isLength({ min: 4, max: 20 }),
+    check("passwordConfirmation")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .custom(async (passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+          throw new Error("Passwords must match");
+        }
+      }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
 
     const { email, password, passwordConfirmation } = req.body;
-
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-      return res.send("Email already in use");
-    }
-
-    if (password !== passwordConfirmation) {
-      return res.send("Passwords must match");
-    }
 
     const user = await usersRepo.create({ email, password });
 
