f529d90db5b23cbecd1b9c552d09c12a3337b05f
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-27 15:44:40 +0600

    12. Validation Around Sign In

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -20,7 +20,10 @@ router.post(
   [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
 
     const { email, password, passwordConfirmation } = req.body;
 
@@ -41,26 +44,46 @@ router.get("/signin", (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/signin",
+  [
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Must Provide a Valid Email")
+      .custom(async (email) => {
+        const user = await usersRepo.getOneBy({ email });
+        if (!user) {
+          throw new Error("Email not found");
+        }
+      }),
 
-  const user = await usersRepo.getOneBy({ email });
+    check("password").trim(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const { email, password } = req.body;
 
-  if (!user) {
-    return res.send("Email not found");
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+      return res.send("Email not found");
+    }
+
+    const validPassword = await usersRepo.comparePassword(
+      user.password,
+      password
+    );
+    if (!validPassword) {
+      return res.send("Invalid Password");
+    }
+
+    req.session.userId = user.id;
+
+    res.send("You are signed in");
   }
-
-  const validPassword = await usersRepo.comparePassword(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send("Invalid Password");
-  }
-
-  req.session.userId = user.id;
-
-  res.send("You are signed in");
-});
+);
 
 module.exports = router;
