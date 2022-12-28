b0457c81c10144b9c62e1cb99ec006618a158540
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-28 18:22:21 +0600

    8. Reusable Error Handling Middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -20,14 +21,9 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
 
     const user = await usersRepo.create({ email, password });
 
@@ -39,7 +35,7 @@ router.post(
 
 router.get("/signout", (req, res) => {
   req.session = null;
-  res.send("You are Logged Out"); 
+  res.send("You are Logged Out");
 });
 
 router.get("/signin", (req, res) => {
@@ -49,13 +45,8 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requiredEmailExist, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
