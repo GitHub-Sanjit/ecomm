63d67f88cbf6819a3ab4e2456ede5faa559c4f29
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-29 23:28:32 +0600

    20. Redirect on Remove

--- routes/carts.js
+++ routes/carts.js
@@ -28,7 +28,7 @@ router.post("/cart/products", async (req, res) => {
     items: cart.items,
   });
 
-  res.send("Product Added to Cart");
+  res.redirect("/cart");
 });
 
 // Receive a Get request to show all items in cart
@@ -50,7 +50,13 @@ router.get("/cart", async (req, res) => {
 
 // Receive a post request to delete an item from a cart
 router.post("/cart/products/delete", async (req, res) => {
-  console.log(req.body.itemId);
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+
+  await cartsRepo.update(req.session.cartId, { items });
+  res.redirect("/cart");
 });
 
 module.exports = router;
