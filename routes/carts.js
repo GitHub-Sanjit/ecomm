1f3aade48de32b8e6ed660624c10f2969c39f7f3
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-29 15:27:43 +0600

    13. Submission Options

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
+// Receive a Post Request to add an item to a cart
+router.post("/cart/products", async (req, res) => {
+  console.log(req.body.productId);
+
+  res.send("Product Added to Cart");
+});
 
 module.exports = router;
