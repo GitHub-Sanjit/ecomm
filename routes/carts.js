4ea7a765c83fd935a50846086e39d2c974635f2c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-29 16:21:05 +0600

    16. Displaying Cart Items

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 // Receive a Post Request to add an item to a cart
@@ -13,12 +15,37 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
-  console.log(cart);
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
 
-  // Either increment quantity for existing product
-  // OR add new product to items array
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
 
   res.send("Product Added to Cart");
+});
+
+// Receive a Get request to show all items in cart
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
 });
 
 module.exports = router;
